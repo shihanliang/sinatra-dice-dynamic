a9fa251440dfd1f236907df5e6a2edf86fd2dfee
shihanliang <sliang1@ChicagoBooth.edu>
2025-04-15 04:09:59 +0000

    code

--- app.rb
+++ app.rb
@@ -2,53 +2,15 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  erb(:homepage)
+  erb(:home)
 end
 
-get("/dice/2/6") do
-  @rolls = []
+get("/dice/:num/:sides") do
+  @num = params[:num].to_i
+  @sides = params[:sides].to_i
 
-  2.times do
-    die = rand(1..6)
+  @rolls = Array.new(@num) { rand(1..@sides) }
+  @title = "#{@num}d#{@sides}"
 
-    @rolls.push(die)
-  end
-
-  erb(:two_six)
-end
-
-get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  @rolls = []
-
-  1.times do
-    die = rand(1..20)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
-
-    @rolls.push(die)
-  end
-
-  erb(:five_four)
+  erb(:dice)
 end
